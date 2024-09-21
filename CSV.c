5670520237517d466bd1f2d18df3915c153b4ca5
Kalina Cathcart <Kcathcart3611@conestogac.on.ca>
2024-09-21 18:42:40 -0400

    Small updates: CSV - changed some magic numbers to constants - fixed printf() in deleteProductLine() to use variable that was passed in - edited some error statements for consistency - renamed some variables to meet SET standards (replacing underscores with camelCase) - perror changed to printf to match other error messages

--- CSV.c
+++ CSV.c
@@ -13,11 +13,14 @@
 #pragma warning(disable: 4996)
 
 
+
 // function prototypes
 void displayFileData(fileName);
 int addNewProduct(char* newProduct, char* filename);
 int deleteProductLine(int lineNumber, char* fileName);
 int updateProduct(int lineNumber, char* fileName, char* newProductInformation);
+
+
 
 // global constants
 #define MAX_STRING_LENGTH 100
@@ -30,21 +33,23 @@ int updateProduct(int lineNumber, char* fileName, char* newProductInformation);
 
 
 
-
 int main(void)
 {
 	
 
-	char fileName[MAX_STRING_LENGTH] = "CSVInventory.csv";	// File pointer to navigate files with 
-	const char* newProductToInsert = "5, Emergency Disco Ball, Food, 85, 29.99";	// Holds string of data to be inserted into CSV file 
-	const char* otherProductToInsert = ",,,,";
-	// Holds string of data to update a current line with in the CSV file 
-	const char* updatedProductInformation = "3, Rival Bracelet, Clothing, 215, 5.99";
+
+	char fileName[MAX_STRING_LENGTH] = "CSVInventory.csv";								// File pointer to navigate files with 
+	const char* newProductToInsert = "5, Emergency Disco Ball, Food, 85, 29.99";		// Holds string of data to be inserted into CSV file 
+	const char* otherProductToInsert = ",,,,";											// Secondary line of data to be inserted, this is used to show empty product data 
+	const char* updatedProductInformation = "3, Rival Bracelet, Clothing, 215, 5.99";	// Holds string of data to update a current line with in the CSV file 
+
+
 
 	//adds two products and then displays them
 	addNewProduct(newProductToInsert, fileName);
 	addNewProduct(otherProductToInsert, fileName);
 	displayFileData(fileName);
+
 
 
 	// Update - modify line 2 existing information  and display again
@@ -60,8 +65,6 @@ int main(void)
 
 
 
-
-
 	// Redisplay data from file after alterations 
 	displayFileData(fileName);
 
@@ -70,32 +73,45 @@ int main(void)
 	return 0;
 
 }
+
+
+
+
+
 // Function Name: checkFormat()
 // Purpose: Makes sure the string passed in has exactly 4 commas. 
 // Parameters: line. String that is going to be checked
 // Return Values: True or False (0,1)
-//
+
 int checkFormat(char* line)
 {
-	int comma_count = 0;
+
+	int commaCount = 0;
 
 	// Loop through the string to count commas
 	for (int i = 0; line[i] != '\0'; i++)
 	{
+
 		if (line[i] == ',')
 		{
-			comma_count++;
+
+			commaCount++;
+
 		}
+
 	}
-	if (comma_count != 4)
+	if (commaCount != NUMBER_OF_COMMAS)
 	{
-		printf("Ensure data is in the proper CSV Format!");
+
+		printf("ERROR: Ensure data is in the proper CSV Format!");
+		return FAILURE;
+
 	}
+
 	// Return true if there are exactly 4 commas, otherwise false 
-	return (comma_count == 4);
+	return (commaCount == NUMBER_OF_COMMAS);
 
 }
-
 
 
 
@@ -103,22 +119,29 @@ int checkFormat(char* line)
 // Purpose: Moves file pointer to beginning of file. Using a while loop, it displays every line present in a file. 
 // Parameters: bufferString to hold pulled data, filepointer to navigate files 
 // Return Values: none
-//
 
 void displayFileData(char* fileName)
 {
+
 	FILE* file = fopen(fileName, "r");
+
 	if (file == NULL)
 	{
+
 		printf("ERROR: The file %s cannot be opened for reading!\n", fileName);
 		return FAILURE;
+
 	}
-	char bufferString[MAX_STRING_LENGTH];
+
+
+
+	char bufferString[MAX_STRING_LENGTH] = "";
+
 	// Move filepointer to the beginning of the file 
 	fseek(file, 0, SEEK_SET);
 
 	// Read and Display all data from file 
-	printf("\nDisplaying Data withing the file: \n");
+	printf("\nDisplaying Data withing the file: %s \n", fileName); 
 
 	// While Loop: As long as there is a line of data to print, the next line will be displayed in the terminal. When there are no more lines of data, the loop ends. 
 	while (fgets(bufferString, sizeof(bufferString), file))
@@ -127,194 +150,290 @@ void displayFileData(char* fileName)
 		printf("\t%s", bufferString);
 
 	}
+
 	printf("\n");
 
 	if (fclose(file) != 0)
 	{
-		printf("Error closing the file: %s\n", fileName);
+
+		printf("ERROR: Problem encountered when attempting to close the file: %s\n", fileName);
+
 	}
+
 }
+
+
 
 // Function Name: addNewProduct()
 // Purpose: This function adds a new product to the end of a .csv file.
 // Parameters: Takes the newProduct to insert, and the file to insert it in
 // Return Values: int: True or False 
-//
+
 int addNewProduct(char* newProduct, char* filename)
 {
+
 	FILE* file = fopen(filename, "a");
+
 	if (file == NULL)
 	{
+
 		printf("ERROR: The file %s cannot be opened for appending!\n", filename);
 		return FAILURE;
+
 	}
 	if (checkFormat(newProduct) != SUCCESS)
 	{
+
 		return FAILURE;
+
 	}
+
+
+
 	printf("Adding \"%s\" to end of file.\n", newProduct);
+
 	// Move the file pointer to the end of the file 
 	fseek(file, 0, SEEK_END);
 
 	// Write in the new line of data into the file
 	fprintf(file, "%s\n", newProduct);
+
 	if (fclose(file) != 0)
 	{
+
 		printf("Error closing the file: %s\n", filename);
+
 	}
 
 	return SUCCESS;
+
 }
+
 
 
 // Function Name: deleteProductLine()
 // Purpose: This function deletes the line specified from a .csv file
 // Parameters: Takes the line to delete and the file to delete it from
 // Return Values: int True or False
-//
+
 int deleteProductLine(int lineNumber, char* filename)
 {
+
 	int currentLineNumber = 0;
 	char buffer[MAX_STRING_LENGTH];
 	FILE* file = fopen(filename, "r");
+
 	if (file == NULL)
 	{
+
 		printf("ERROR: The file %s cannot be opened for reading!\n", filename);
 		return FAILURE;
+
 	}
+
 	FILE* tempFile = tmpfile();
+
 	if (tempFile == NULL)
 	{
-		printf("ERROR: Could create a temporary file!\n", filename);
+
+		printf("ERROR: Could not create a temporary file for %s!\n", filename);
+
 		if (fclose(file) != 0)
 		{
+
 			printf("Error closing the file: %s\n", filename);
+
 		}
+
 		return FAILURE;
+
 	}
+
 	//Copy the file into a temporary file, but don't copy the specified line
 	while (fgets(buffer, MAX_STRING_LENGTH, file))
 	{
+
 		currentLineNumber++;
-		if (currentLineNumber == lineNumber)
+
+		if (currentLineNumber == lineNumber)  //don't copy deleted line
 		{
-			continue;  //don't copy deleted line
+
+			continue;  
+
 		}
-		else
+		else  //keep other lines the same
 		{
-			fputs(buffer, tempFile); //keep other lines the same
+
+			fputs(buffer, tempFile); 
+
 		}
 	}
-	if (currentLineNumber < lineNumber) {
+
+	if (currentLineNumber < lineNumber) 
+	{
+
 		printf("Error: Specified line number does not exist!\n");
 ;
 		if (fclose(tempFile) != 0)
 		{
+
 			printf("Error closing the temporary file");
+
 		}
 
 		if (fclose(file) != 0)
 		{
+
 			printf("Error closing the file: %s\n", filename);
+
 		}
+
 		return FAILURE;
+
 	}
 
 
 	if (fclose(file) != 0)
 	{
+
 		printf("Error closing the file: %s\n", filename);
+
 	}
 
 
 
 	file = fopen(filename, "w"); 
+
 	if (file == NULL)
 	{
+
 		printf("ERROR: The file %s cannot be opened for writing!\n", filename);
 		return FAILURE;
+
 	}
+
 	rewind(tempFile); //clear End of file flag and reset the file pointer to beginning
 
 	//Copy the content of the temporary file back into the original file
 	while (fgets(buffer, MAX_STRING_LENGTH, tempFile))
 	{
+
 		fputs(buffer, file);
+
 	}
 
 	if (fclose(file) != 0)
 	{
-		printf("Error closing the file: %s\n", filename);
+
+		printf("ERROR: Problem encountered when attempting to close the file: %s\n", filename);
+
 	}
 	if (fclose(tempFile) != 0)
 	{
-		printf("Error closing the temporary file\n");
+
+		printf("ERROR: Problem encountered when attempting to close the temporary file\n");
+
 	}
 
-
 	return;
+
 }
+
+
 
 // Function Name: updateProduct()
 // Purpose: Change a product line anywhere in the csv file to updated information
 // Parameters: lineNumber to delete, filename to delete from, and the string to replace it.
 // Return Values: int True or False (pass or fail)
-//
+
 int updateProduct(int lineNumber, char* filename, char* newProductInformation)
 {
+
 	int currentLineNumber = 0;
 	char buffer[MAX_STRING_LENGTH];
+
+
+
 	FILE* file = fopen(filename, "r");
 	if (file == NULL)
 	{
+
 		printf("ERROR: The file %s cannot be opened for reading!\n", filename);
 		return FAILURE;
+
 	}
+
+
+
 	FILE* tempFile = tmpfile();
 	if (tempFile == NULL)
 	{
-		printf("ERROR: Could create a temporary file!\n", filename);
+		printf("ERROR: Could not create a temporary file\n");
+
 		if (fclose(file) != 0)
 		{
-			printf("Error closing the file: %s\n", filename);
+
+			printf("ERROR: Problem encountered when attempting to close the file: %s\n", filename);
+
 		}
+
 		return FAILURE;
+
 	}
+
+
 
 	//copy original file into a temporary file
 	while (fgets(buffer, MAX_STRING_LENGTH, file))
 	{
+
 		currentLineNumber++;
+
 		if (currentLineNumber == lineNumber)  //If the line number is the one specified
 		{
+
 			if (checkFormat(newProductInformation)) //make sure new information is formatted correctly
 			{
+
 				fprintf(tempFile, "%s\n", newProductInformation); //write the new information to the file
+
 			}
 			else
 			{
+
 				fputs(buffer, tempFile); //if the format is wrong, keep the old information
+
 			}
+
 		}
 		else
 		{
+
 			fputs(buffer, tempFile); //keep other lines the same
+
 		}
 	}
 	//If Line number never reaches the one specified, they tried to change a line that doesn't exist
-	if (currentLineNumber < lineNumber) {
-		printf("Error: Specified line number does not exist!\n");
+	if (currentLineNumber < lineNumber) 
+	{
+
+		printf("ERROR: Specified line number does not exist!\n");
 		fclose(tempFile);
 		fclose(file);
 		return FAILURE;
+
 	}
 
 	fclose(file);
+
 	file = fopen(filename, "w"); //reopen original file this time to overwrite
+
 	if (file == NULL)
 	{
+
 		printf("ERROR: The file %s cannot be opened for writing!\n", filename);
 		return FAILURE;
+
 	}
 
 	rewind(tempFile); //reset the file pointer
@@ -322,19 +441,27 @@ int updateProduct(int lineNumber, char* filename, char* newProductInformation)
 	//copy contents from the temporary file into the original file (with the changes)
 	while (fgets(buffer, MAX_STRING_LENGTH, tempFile))
 	{
+
 		fprintf(file, buffer);
+
 	}
 
 
 	if (fclose(file) != 0)
 	{
-		printf("Error closing the file: %s\n", filename);
+
+		printf("ERROR: Problem encountered when attempting to close the file: %s\n", filename);
+
 	}
 
 	if (fclose(tempFile) != 0)
 	{
-		perror("Error closing file");
+
+		printf("ERROR: Problem encountered when attempting to close the temporary file\n"); 
+
 	}
+
 	return SUCCESS;
+
 }
 
